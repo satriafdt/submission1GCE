f5b63b2f5edaa8e051b3bb9a1c6d567ee314d426
Satria Finandita <satria.fdt@gmail.com>
2022-06-19 01:33:26 +0700

    First Commit, for submission Dicoding

---
+++ index.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.88.1">
+  <title>My Website</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/starter-template/">
+
+  <!-- Bootstrap core CSS -->
+  <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+
+
+  <!-- Custom styles for this template -->
+  <link href="assets/dist/css/starter-template.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">My Website</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+
+  <main role="main" class="container">
+
+    <div class="row">
+      <div class="col-xl-12">
+        <!-- Profile picture card-->
+        <div class="card mb-12 mb-xl-0">
+          <div class="card-header">
+            My Personal Information
+          </div>
+          <div class="card-body text-center">
+            <!-- Profile picture image-->
+            <img class="img-account-profile rounded-circle mb-2" src="https://storage.googleapis.com/digitalent_satriafdt/profile.jpg" alt="Profile Image" width="144" height="144">
+            <div class="big font-strong mb-12">Satria Finandita</div>
+            <div class="small font-italic text-muted mb-12">Junior Web Developer</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="card mb-12 mb-xl-0">
+          <div class="card-header">My Story</div>
+          <div class="card-body text-justify">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer erat magna, congue non congue sit amet,
+              laoreet quis nunc. Proin ut faucibus massa, eget accumsan nulla. Mauris ultrices viverra tempus. Phasellus
+              blandit placerat ipsum vel sodales.
+            </p>
+            <p>Maecenas sit amet tellus imperdiet, malesuada sem ac, lacinia odio. Ut et eros eget magna
+              tristique interdum at nec elit. Proin eu dui a velit malesuada blandit quis eget ex. Nunc quis dictum
+              elit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="card mb-12 mb-xl-0">
+          <div class="card-header">Personal Information</div>
+          <div class="card-body px-0">
+            <!-- Payment method 1-->
+            <div class="d-flex align-items-center justify-content-between px-4">
+              <div class="d-flex align-items-center">
+                <div class="ms-12">
+                  <div class="small">Age</div>
+                  <div class="text-xs text-muted">28 years old</div>
+                </div>
+              </div>
+            </div>
+            <hr>
+            <div class="d-flex align-items-center justify-content-between px-4">
+              <div class="d-flex align-items-center">
+                <div class="ms-12">
+                  <div class="small">Phone</div>
+                  <div class="text-xs text-muted">+62 812-78XX-XXX</div>
+                </div>
+              </div>
+            </div>
+            <hr>
+            <div class="d-flex align-items-center justify-content-between px-4">
+              <div class="d-flex align-items-center">
+                <div class="ms-12">
+                  <div class="small">E-Mail</div>
+                  <div class="text-xs text-muted">satria.finandita@dicoding.com</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card mb-12 mb-xl-0">
+          <div class="card-header text-center">Find me in!</div>
+          <div class="card-body text-center">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="https://github.com/satriafdt">
+                  <img class="rounded-circle mb-2" src="https://storage.googleapis.com/digitalent_satriafdt/logo-github.png" alt="Github" width="60" height="60">
+                </a>
+              </div>
+              <div class="col-sm-4">
+                <a href="https://www.linkedin.com/in/satria-finandita-4a9909118">
+                  <img class="rounded-circle mb-2" src="https://storage.googleapis.com/digitalent_satriafdt/logo-linkedin.png" alt="Linkedin" width="60" height="60">
+                </a>
+              </div>
+              <div class="col-sm-4">
+                <a href="https://twitter.com/satriafdt">
+                  <img class="rounded-circle mb-2" src="https://storage.googleapis.com/digitalent_satriafdt/logo-twitter.png" alt="Twitter" width="60" height="60">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </main><!-- /.container -->
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script>
+    window.jQuery || document.write('<script src="assets/js/vendor/jquery.slim.min.js"><\/script>')
+  </script>
+  <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
